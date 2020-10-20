e21c3bf71bfa724f3e6c56deda57549a4075cc38
bokor-git <bogdankorolbk@gmail.com>
2020-10-20 16:35:00 +0300

    Updates

---
+++ precache-manifest.20d1fabd41d8775f6737cdd501affdc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b169c4956c3bd22cd7eeaeb901df8fa",
+    "url": "/chat-websoket/index.html"
+  },
+  {
+    "revision": "840dce6a7a8b48165421",
+    "url": "/chat-websoket/static/css/main.5d23529a.chunk.css"
+  },
+  {
+    "revision": "413f7ebe9e7eb286bcf9",
+    "url": "/chat-websoket/static/js/2.e535a202.chunk.js"
+  },
+  {
+    "revision": "9b708884f5d885b064e66ca0a4b65934",
+    "url": "/chat-websoket/static/js/2.e535a202.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "840dce6a7a8b48165421",
+    "url": "/chat-websoket/static/js/main.2f1d93fd.chunk.js"
+  },
+  {
+    "revision": "2a5f43b3eef49e519ad2",
+    "url": "/chat-websoket/static/js/runtime-main.db7d454e.js"
+  }
+]);
